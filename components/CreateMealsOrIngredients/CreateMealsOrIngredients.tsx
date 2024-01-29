46ad1f2517872ff38dfed7adbdeacaa668e28bb9
Pawel <pawel.bornikowski@gmail.com>
2024-01-29 21:29:31 +0100

    styles: display nutrition of ingredients
    create containers for each ingredient and style all content inside

--- components/CreateMealsOrIngredients/CreateMealsOrIngredients.tsx
+++ components/CreateMealsOrIngredients/CreateMealsOrIngredients.tsx
@@ -21,25 +21,36 @@ const CreateMealsOrIngredients = () => {
   console.log(nutrition[0]);
   return (
     <div className="h-full">
-      <div className="border p-4 rounded-3xl h-full">
-        <form action={action}>
+      <div className=" p-4 rounded-3xl h-full bg-[#f8f8ff]">
+        <form action={action} className="flex gap-4">
           <Input name="prompt" />
-          <Button variant="outline">Add</Button>
+          <Button variant="outline" className="w-[100px]">Check</Button>
         </form>
 
-        <ul>
-            {nutrition.map((item) => (
-                <ul>
-                    <li>Name: {item.name}</li>
-                    <li>Amount: {item.serving_size_g}</li>
-                    <li>Calories: {item.calories}</li>
-                    <li>Protein: {item.protein_g}</li>
-                    <li>Fat: {item.fat_total_g}</li>
-                    <li>Carbs: {item.carbohydrates_total_g}</li>
-                    <li>Sugar: {item.sugar_g}</li>
-                </ul>
-            ))}
-        </ul>
+        <div className="flex flex-col w-full gap-4 mt-12 ">
+          {nutrition.map((item) => (
+            <div className="w-full rounded-xl  p-4 grid grid-cols-3 items-center bg-white ">
+                <div className=" flex flex-col justify-center items-center">
+                    <h2 className="text-2xl">{item.name}</h2>
+                    <p className="text-muted-foreground">{item.serving_size_g}g</p>
+                </div>
+              <div>
+                <div className="w-[100px] h-[100px] rounded-full  border-2 flex justify-center items-center flex-col">
+                    
+                    <span>{item.calories}</span>
+                    <span>Cal</span>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-2 rounded-2xl gradient font-bold p-4 text-[#dacafb]">
+                    <span>P: {item.protein_g}g</span>
+                    <span>F: {item.fat_total_g}g</span>
+                    <span>C: {item.carbohydrates_total_g}g</span>
+                    <span>S: {item.sugar_g}g</span>
+              </div>
+              
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -18,6 +18,12 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -35,6 +41,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
