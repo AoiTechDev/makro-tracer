fcfdbb1b3e0641984ca9e7f5ca565baa0fdd0da9
Pawel <pawel.bornikowski@gmail.com>
2024-02-02 12:41:47 +0100

    chore: add calories input and change layout
    improve layout to be more handy and ann calories input to add meal seciton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,38 +16,48 @@ const page = async () => {
   }
   return (
     // <Card className="flex flex-1 ">
-     
+
     //   asd
     // </Card>
 
-<Card className="flex-1">
-<CardHeader>
-  <CardTitle>Add Meal</CardTitle>
-</CardHeader>
-<CardContent>
-  <form className="space-y-4">
-    <div className="space-y-2">   
-      <Label htmlFor="meal-name">Meal Name</Label>
-      <Input id="meal-name" required />
-    </div>
-    <div className="space-y-2">
-      <Label htmlFor="protein">Protein (g)</Label>
-      <Input id="protein" required type="number" />
-    </div>
-    <div className="space-y-2">
-      <Label htmlFor="carbs">Carbohydrates (g)</Label>
-      <Input id="carbs" required type="number" />
-    </div>
-    <div className="space-y-2">
-      <Label htmlFor="fat">Fat (g)</Label>
-      <Input id="fat" required type="number" />
-    </div>
-    <Button className="w-full" type="submit">
-      Add Meal
-    </Button>
-  </form>
-</CardContent>
-</Card>
+    <Card className="flex-1">
+      <CardHeader>
+        <CardTitle>Add Meal</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="meal-name">Meal Name</Label>
+            <Input id="meal-name" required />
+          </div>
+          <div className="flex gap-4">
+            <div className="space-y-2 flex-1 ">
+              <Label htmlFor="calories">Calories</Label>
+              <Input id="calories" required />
+            </div>
+            <div className="space-y-2 flex-1">
+              <Label htmlFor="protein">Protein (g)</Label>
+              <Input id="protein" required type="number" />
+            </div>
+          </div>
+
+          <div  className="flex gap-4">
+            <div className="space-y-2 flex-1">
+              <Label htmlFor="carbs">Carbohydrates (g)</Label>
+              <Input id="carbs" required type="number" />
+            </div>
+            <div className="space-y-2 flex-1">
+              <Label htmlFor="fat">Fat (g)</Label>
+              <Input id="fat" required type="number" />
+            </div>
+          </div>
+
+          <Button className="w-full" type="submit">
+            Add Meal
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 
