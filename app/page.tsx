aa5c91d929fd23bf4fe6de10c7ec4f67a9cf073f
Pawel <pawel.bornikowski@gmail.com>
2024-02-09 02:02:59 +0100

    feat: add header, subheader, button and image on landpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 import { redirect } from "next/navigation";
+import landpageImg from "@/assets/landing-page.png";
+import spiral from "@/assets/spiral.png";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerSession();
@@ -7,8 +11,25 @@ export default async function Home() {
     redirect("/login");
   }
   return (
-    <main className=" flex justify-center items-center w-screen h-screen">
-      Welcome to Macro Tracker
+    <main className="w-full h-screen  grid grid-cols-2 px-24 max-[1300px]:grid-cols-1 items-center">
+      <div className="relative w-full flex justify-center items-start gap-5 flex-col">
+        {/* <Image
+          alt="landpage img"
+          fill
+          src={spiral}
+        /> */}
+        <h1 className="text-8xl"><span className="text-[#4176F4]">Nourish</span> Your Body, Delight Your <span className="text-[#4176F4]">Palate</span></h1>
+        <p className="text-xl text-balance">Empower Yourself with Comprehensive Meal Tracking: Easily Create, Add and Search for Meals to Stay On Track.</p>
+        <Button className="w-[200px] h-[50px] text-xl">Discover</Button>
+      </div>
+      <div className=" aspect-square relative w-full max-[1300px]:hidden  ">
+        <Image
+          alt="landpage img"
+          fill
+          src={landpageImg}
+          className=""
+        />
+      </div>
     </main>
   );
 }
