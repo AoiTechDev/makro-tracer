f00ba6da8c247a8b3b2c0064af3ee19cd522c025
Pawel <pawel.bornikowski@gmail.com>
2024-02-09 15:06:43 +0100

    chore: prepare delete buton on meals

--- components/MealsTableBody/MealsTableRow/MealsTableRow.tsx
+++ components/MealsTableBody/MealsTableRow/MealsTableRow.tsx
@@ -8,25 +8,30 @@ import React, { useEffect } from "react";
 type MealsTableRowProps = {
   result: GetMealsResponse;
 };
-
+//TODO 
+//There need to me sql query to fetch just meals for this date, or just for current month or week to not map over every meals couse it might slow down app
 const MealsTableRow = ({ result }: MealsTableRowProps) => {
   const { date } = useCalendarStore();
 
   const formattedOriginalDate = formattedDate(date)
+  console.log('################################')
   {
     return result?.success
       ? result.success.rows.map((row) => {
           const rowDate = new Date(row.date);
           const formattedRowDate = formattedDate(rowDate)
+         
+          console.log(row.mealid)
           return (
             formattedOriginalDate === formattedRowDate && (
-              <TableRow className=" ">
+              <TableRow  key={row.mealid} className="relative group ">
                 <TableCell className="font-medium">{row.name}</TableCell>
                 <TableCell>{row.calories}</TableCell>
                 <TableCell>{row.protein}</TableCell>
                 <TableCell>{row.carbohydrates}</TableCell>
                 <TableCell>{row.fat}</TableCell>
                 <TableCell>{row.sugar}</TableCell>
+                <div className="absolute w-5 h-5 bg-red-400 right-2 top-[50%] -translate-y-[50%] group-hover:block hidden"></div>
               </TableRow>
             )
           );
