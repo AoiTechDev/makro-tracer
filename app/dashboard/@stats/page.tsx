c777a204b5be57447dc0403c3adbbe3d4f08500d
Pawel <pawel.bornikowski@gmail.com>
2024-02-15 20:00:32 +0100

    chore: create paraller route for stats

---
+++ app/dashboard/@stats/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import React from "react";
+
+const page = () => {
+  return (
+    <Card className="min-h-[400px] flex-1 bg-red-200 lg:flex justify-center items-center hidden">
+      Existing meals(comming soon)
+    </Card>
+  );
+};
+
+export default page;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,23 +5,24 @@ const DashboardLayout = ({
   children,
   table,
   calendar,
+  stats,
 }: Readonly<{
   children: React.ReactNode;
   calendar: React.ReactNode;
   table: React.ReactNode;
+  stats: React.ReactNode;
 }>) => {
   return (
     <div className="flex flex-col lg:flex-row gap-4 justify-around w-full p-2 md:p-4">
-     
       <div className="w-full lg:w-3/4 flex flex-col gap-4 order-2 lg:order-1">
         <div className="flex flex-col lg:flex-row gap-4 ">
           {children}
-          <Card className="min-h-[400px] flex-1 bg-red-200 lg:flex justify-center items-center hidden">Existing meals(comming soon)</Card>
+         {stats}
         </div>
         {table}
       </div>
 
-     {calendar}
+      {calendar}
     </div>
   );
 };
