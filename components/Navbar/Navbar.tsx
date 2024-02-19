a72a8d66badc1e8a879596b00df71e2c7b2f36ea
Pawel <pawel.bornikowski@gmail.com>
2024-02-19 17:27:11 +0100

    feat: add logo to nav

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,15 +5,18 @@ import Login from "./Login/Login";
 import Register from "./Register/Register";
 
 import Profile from "./Profile/Profile";
-
+import logo from "@/assets/logo.png";
+import Image from "next/image";
 const Navbar = async () => {
   const session = await getServerSession();
 
   return (
-    <nav className="fixed  bg-white flex border-b w-full h-16 top-0 items-center z-50 justify-end px-6 lg:px-12">
+    <nav className="fixed  bg-white flex border-b w-full h-16 top-0 items-center z-50 justify-between px-6 ">
+      <div className="w-[180px] h-[100px] flex items-center justify-center lg:w-[250px] lg:h-[100px]  ">
+        <Image src={logo}  alt="logo" />
+      </div>
       {!!session && (
         <div className="flex gap-4">
-        
           <Profile session={session} />
         </div>
       )}
