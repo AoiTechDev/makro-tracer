393de183275e86f7d6cc71bf7b84cc7acb72067b
Pawel <pawel.bornikowski@gmail.com>
2024-02-28 01:26:49 +0100

    feat: full screen on mobile
    also add close button to header

--- components/Assistant/Overlay/Overlay.tsx
+++ components/Assistant/Overlay/Overlay.tsx
@@ -8,15 +8,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import { RxCross2 } from "react-icons/rx";
 const Overlay = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       {isOpen ? (
-        <Card className="fixed bottom-20 right-0 lg:right-3 border bg-white max-w-[400px] h-[90dvh] lg:h-[650px] flex flex-col justify-between pb-2 z-50">
-          <CardHeader>
+        <Card className="fixed bottom-0 lg:bottom-20 right-0 lg:right-3 border bg-white max-w-[400px] h-[100dvh] lg:h-[650px] flex flex-col justify-between pb-2 z-50">
+          <CardHeader className="relative">
             <CardTitle>
               <span className=" bg-gradient-to-r from-indigo-500 to-purple-500 inline-block text-transparent bg-clip-text">
                 Mealfull
@@ -24,6 +24,12 @@ const Overlay = () => {
               Assistant
             </CardTitle>
             <CardDescription>Ask for anything!</CardDescription>
+            <div
+              className="top-5 right-5 w-5 h-5 absolute flex items-center justify-center cursor-pointer"
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
+              <RxCross2 className="text-6xl" />
+            </div>
           </CardHeader>
           <MessagesArea />
           <ChatInput />
