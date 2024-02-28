e53b7d3c3516c480565d591a59144694a69f6704
Pawel <pawel.bornikowski@gmail.com>
2024-02-28 02:01:03 +0100

    styles: add hover effect

--- components/Assistant/Overlay/Overlay.tsx
+++ components/Assistant/Overlay/Overlay.tsx
@@ -37,7 +37,7 @@ const Overlay = () => {
         </Card>
       ) : null}
       <div
-        className="fixed bottom-3 right-3 border-2 w-12 h-12 rounded-full flex justify-center items-center p-[2px] bg-gradient-to-r from-indigo-500 to-purple-500"
+        className='fixed bottom-3 right-3 w-12 h-12 rounded-full hidden md:flex justify-center items-center p-[2px] bg-gradient-to-r from-indigo-500 to-purple-500 overlay-circle hover:scale-110 duration-200'
         onClick={() => setIsOpen((prev) => !prev)}
       >
         <button className="rounded-full w-full h-full flex bg-white justify-center items-center ">
