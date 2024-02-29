3c7a6e0210ebb14c7b08863a0ac057f2c9ba4002
Pawel <pawel.bornikowski@gmail.com>
2024-02-29 12:40:43 +0100

    feat: display ai overlay only inside dashboard

--- components/Assistant/Overlay/Overlay.tsx
+++ components/Assistant/Overlay/Overlay.tsx
@@ -1,20 +1,20 @@
 "use client";
 import React, { useState } from "react";
 
-
 import OverlayChatArea from "./OverlayChatArea";
 import OverlayButton from "./OverlayButton";
+import { usePathname } from "next/navigation";
 
 const Overlay = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  return (
+  const pathname = usePathname();
+  return pathname === '/dashboard' ? (
     <>
       {isOpen ? <OverlayChatArea setIsOpen={setIsOpen} /> : null}
 
       <OverlayButton setIsOpen={setIsOpen} />
     </>
-  );
+  ): null;
 };
 
 export default Overlay;
