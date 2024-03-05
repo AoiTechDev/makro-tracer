af6b741a550448692b79643640f5e9c48583b23e
Pawel <pawel.bornikowski@gmail.com>
2024-03-05 17:20:55 +0100

    feat: add edit image button with dropdown

--- components/Settings/SettingsContent/ProfileImage.tsx
+++ components/Settings/SettingsContent/ProfileImage.tsx
@@ -1,19 +1,42 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Label } from '@/components/ui/label'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import SVGPencil from "./SVGPencil";
+import { Card } from "@/components/ui/card";
 
 const ProfileImage = () => {
   return (
     <div className="flex-1 w-full flex flex-col items-center">
-    <div>
-      <Label>Profile Picture</Label>
-      <Avatar className="border h-48 w-48">
-        <AvatarImage alt="Avatar image" src="/placeholder-user.jpg" />
-        <AvatarFallback>JD</AvatarFallback>
-      </Avatar>
+      <div className="relative">
+        <Label>Profile Picture</Label>
+        <Avatar className="border h-48 w-48">
+          <AvatarImage alt="Avatar image" src="/placeholder-user.jpg" />
+          <AvatarFallback>JD</AvatarFallback>
+        </Avatar>
+        <Popover>
+          <PopoverTrigger className="absolute bottom-3 left-0">
+            <Card className=" flex px-2 py-1 justify-center items-center gap-1">
+              <SVGPencil />
+              Edit
+            </Card>
+          </PopoverTrigger>
+          <PopoverContent className="flex flex-col gap-4 max-w-40">
+            <Label className="cursor-pointer" htmlFor="profile-image">
+              Upload a photo...
+            </Label>
+            <Label className="cursor-pointer">Remove photo</Label>
+            <Input type="file" id="profile-image" className="hidden" />
+          </PopoverContent>
+        </Popover>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ProfileImage
+export default ProfileImage;
