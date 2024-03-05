b8e84c48ddd535cc4e0cc02b4eccc399050df53e
Pawel <pawel.bornikowski@gmail.com>
2024-03-05 17:34:19 +0100

    chore: read uploaded file and display as avatar

--- components/Settings/SettingsContent/ProfileImage.tsx
+++ components/Settings/SettingsContent/ProfileImage.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import React, { useState } from "react";
 import {
   Popover,
   PopoverContent,
@@ -11,13 +12,42 @@ import SVGPencil from "./SVGPencil";
 import { Card } from "@/components/ui/card";
 
 const ProfileImage = () => {
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setFile(file);
+
+    if (fileUrl) {
+      URL.revokeObjectURL(fileUrl);
+    }
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+    } else {
+      setFileUrl(undefined);
+    }
+  };
+
   return (
     <div className="flex-1 w-full flex flex-col items-center">
       <div className="relative">
         <Label>Profile Picture</Label>
         <Avatar className="border h-48 w-48">
-          <AvatarImage alt="Avatar image" src="/placeholder-user.jpg" />
-          <AvatarFallback>JD</AvatarFallback>
+          {file && fileUrl ? (
+            <>
+              <AvatarImage alt="Avatar image" src={fileUrl} className="object-cover"/>
+              <AvatarFallback>JD</AvatarFallback>
+            </>
+          ) : (
+            <>
+              <AvatarImage alt="Avatar image" src="/placeholder-user.jpg" />
+              <AvatarFallback>JD</AvatarFallback>
+            </>
+          )}
+          {/* <AvatarImage alt="Avatar image" src="/placeholder-user.jpg" />
+          <AvatarFallback>JD</AvatarFallback> */}
         </Avatar>
         <Popover>
           <PopoverTrigger className="absolute bottom-3 left-0">
@@ -31,7 +61,13 @@ const ProfileImage = () => {
               Upload a photo...
             </Label>
             <Label className="cursor-pointer">Remove photo</Label>
-            <Input type="file" id="profile-image" className="hidden" />
+            <Input
+              type="file"
+              id="profile-image"
+              className="hidden"
+              accept="image/ipeg,image/png,image/webp"
+              onChange={handleFileChange}
+            />
           </PopoverContent>
         </Popover>
       </div>
