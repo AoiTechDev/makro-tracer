c3d024eb78bf2b5ee47b4c8739d5dfa9642bca24
Pawel <pawel.bornikowski@gmail.com>
2024-03-12 01:42:38 +0100

    feat: add blur bg on landpage, remove learn more btn

--- components/Landpage/Features.tsx
+++ components/Landpage/Features.tsx
@@ -17,7 +17,19 @@ const Features = () => {
     once: true
   })
   return (
-    <div  className="flex items-center justify-center flex-col">
+    <div  className="flex items-center justify-center flex-col  relative">
+       <div
+          aria-hidden="true"
+          className="pointer-events-none absolute inset-x-0 top-10 -z-10  overflow-hidden blur-3xl sm:left-10 sm:top-52"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30  sm:left-[calc(50%-15rem)] sm:w-[72.1875rem]"
+          />
+        </div>
       <motion.h2 ref={ref}
       initial={{opacity: 0, y: 50}}
       animate={isInView ? {opacity: 1, y: -20} : {opacity: 0, y: 50}}
--- components/Landpage/Hero.tsx
+++ components/Landpage/Hero.tsx
@@ -14,6 +14,19 @@ const Hero = ({ session }: HeroProps) => {
   return (
     <div className="grid grid-cols-2 max-[900px]:grid-cols-1 items-center lg:pl-12 max-[900px]:mt-24 lg:mt-0">
       <div className="relative w-full flex justify-center items-start max-[900px]:items-center  gap-8 flex-col p-4  max-[900px]:text-center">
+        <div
+          aria-hidden="true"
+          className="pointer-events-none absolute inset-x-0 top-50 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-20"
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30  sm:left-[calc(50%-15rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      
         <motion.h1
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
@@ -40,20 +53,22 @@ const Hero = ({ session }: HeroProps) => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2 }}
-          className="space-y-4 md:space-x-4"                
+          className="space-y-4 md:space-x-4"
         >
           <Link href={session ? "/dashboard" : "/register"}>
             <ButtonWithBorder text="Discover" size="lg" />
           </Link>
 
-          <ButtonWithBorder text="Learn more" variant="outline" size="lg" />
+          {/* <ButtonWithBorder text="Learn more" variant="outline" size="lg" /> */}
         </motion.div>
       </div>
+      
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         className=" aspect-square relative w-full   "
       >
+ 
         <Image alt="landpage img" fill src={landpageImg} className="" />
       </motion.div>
     </div>
