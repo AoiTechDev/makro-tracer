fe8a3c9fb506db62ec6cb186725cb0eb7a6913d3
Pawel <pawel.bornikowski@gmail.com>
2024-03-13 21:52:46 +0100

    fix: add hostname in next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'local-mealfulness.s3.eu-central-1.amazonaws.com',
+            hostname: 'prod-mealfulness.s3.eu-central-1.amazonaws.com',
             port: '',
             pathname: '/**',
           },
