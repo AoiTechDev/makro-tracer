ee560e0e3c1a009ad26ed161c3e89ec85cf55d7b
Pawel <pawel.bornikowski@gmail.com>
2024-03-21 00:26:25 +0100

    feat: add animation to preview

--- components/Landpage/ImageSection.tsx
+++ components/Landpage/ImageSection.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import preview from "@/assets/dashboard-preview.png";
+import { motion } from "framer-motion";
 const ImageSection = () => {
   return (
-    <div className="mx-auto max-w-6xl px-6 lg:px-8">
-        
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.6 }}
+      viewport={{ once: true, amount: 0.5 }}
+      className="mx-auto max-w-6xl px-6 lg:px-8 mt-24 mb-24"
+    >
       <div className="mt-16 flow-root sm:mt-24">
         <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
           <Image
@@ -17,7 +24,7 @@ const ImageSection = () => {
           />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
