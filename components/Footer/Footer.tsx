306929b93b9cb69afcb76ac529c6e14283001553
Pawel <pawel.bornikowski@gmail.com>
2024-03-21 00:27:04 +0100

    styles: fix footer width

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ import { Separator } from "@/components/ui/separator";
 const Footer = () => {
   return (
     <footer className="w-full   flex justify-center items-center flex-col mt-12 ">
-      <Separator className="w-3/4" />
+      <Separator className="w-full" />
 
-      <div className="flex justify-between items-center py-6 w-3/4 text-sm opacity-60 flex-col gap-6 lg:gap-0 lg:flex-row">
+      <div className="flex justify-between items-center py-6 w-full text-sm opacity-60 flex-col gap-6 lg:gap-0 lg:flex-row">
         <div>
           <p> &copy;{new Date().getFullYear()} Mealfulness. All Right Reserved.</p>
         </div>
