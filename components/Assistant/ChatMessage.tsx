1e9e70a68caa6c7f75b146b4f6c2ff079e8ab8e0
Pawel <pawel.bornikowski@gmail.com>
2024-03-26 18:49:35 +0100

    feat: add user avatar inside chat

--- components/Assistant/ChatMessage.tsx
+++ components/Assistant/ChatMessage.tsx
@@ -1,7 +1,8 @@
 import { cn } from "@/lib/utils";
-import { useSessionStore } from "@/store/store";
+import { useSessionStore, useUserStore } from "@/store/store";
 import { FC, HTMLAttributes } from "react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
+import UserAvatar from "../reusable/UserAvatar";
 
 interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -17,22 +18,23 @@ const ChatMessage: FC<ChatMessagesProps> = ({
   const avatarFallback = `${userSession?.user?.email
     ?.split("")[0]
     ?.toUpperCase()}${userSession?.user?.email?.split("")[1]?.toUpperCase()}`;
+  const { avatar } = useUserStore();
   return (
     <div className={cn("flex", className)} {...props}>
-     
-      <Avatar className={cn('',{
-        "order-2": isUserMessage,
-      
-      })}>
-        <AvatarFallback
-          className={cn(" flex-shrink-0")}
-        >
-          {isUserMessage ? avatarFallback : "AI"}
+      <Avatar
+        className={cn("", {
+          "order-2": isUserMessage,
+        })}
+      >
+        <AvatarFallback className={cn(" flex-shrink-0")}>
+          {isUserMessage ? <UserAvatar image={avatar} /> : "AI"}
         </AvatarFallback>
       </Avatar>
+
       <p
         className={cn("bg-gray-100 rounded-xl p-4 text-sm rounded-r-[20px]", {
-          "bg-gradient-to-r from-purple-500 to-indigo-500 text-white rounded-l-[20px] ": isUserMessage,
+          "bg-gradient-to-r from-purple-500 to-indigo-500 text-white rounded-l-[20px] ":
+            isUserMessage,
           "bg-gray-100 text-gray-900 rounded-r-[20px]": !isUserMessage,
         })}
       >
