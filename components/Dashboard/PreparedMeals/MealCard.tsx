c57f81a0436637b4b45ff1abf35a9c5a98594c29
Pawel <pawel.bornikowski@gmail.com>
2024-04-19 23:41:00 +0200

    feat: add expanding card func

--- components/Dashboard/PreparedMeals/MealCard.tsx
+++ components/Dashboard/PreparedMeals/MealCard.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
 import { QueryResultRow } from "@vercel/postgres";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
@@ -16,10 +10,22 @@ import { Button } from "@/components/ui/button";
 import { TiPlus } from "react-icons/ti";
 import AddMealButton from "../CreateMeal/AddMealButton";
 const MealCard = ({ meals }: { meals: QueryResultRow[] | undefined }) => {
-  const mealList = meals?.map((meal) => (
+  const cardRefs = useRef<Array<HTMLDivElement | null>>([]);
+
+  const handleExpandCard = (index: number) => {
+    if (cardRefs.current[index]?.classList.contains("activeCard")) {
+      setTimeout(() => {
+        cardRefs.current[index]?.classList.remove("activeCard");
+      }, 10);
+    }
+
+    cardRefs.current[index]?.classList.add("activeCard");
+  };
+  const mealList = meals?.map((meal, index) => (
     <Card
       key={meal.mealid}
       className="max-h-[120px] overflow-hidden  relative group"
+      ref={(el) => (cardRefs.current[index] = el)}
     >
       <CardHeader className="space-y-0 ">
         <div>{meal.name}</div>
@@ -30,7 +36,10 @@ const MealCard = ({ meals }: { meals: QueryResultRow[] | undefined }) => {
         <TiPlus className=" text-xl lg:text-2xl text-green-600 cursor-pointer" />
         <MdEdit className=" text-xl lg:text-2xl text-black cursor-pointer" />
         <MdDelete className=" text-xl lg:text-2xl text-red-600 cursor-pointer" />
-        <IoIosArrowDown className="text-xl lg:text-2xl text-black cursor-pointer" />
+        <IoIosArrowDown
+          className="text-xl lg:text-2xl text-black cursor-pointer"
+          onClick={() => handleExpandCard(index)}
+        />
       </div>
     </Card>
   ));
