fbd96bb2b1fec79385e439eb92f1f77af68db46f
Pawel <pawel.bornikowski@gmail.com>
2024-04-19 23:34:37 +0200

    feat: add plus and arrow icon to prepared meal card

--- components/Dashboard/CreateMeal/AddMealButton.tsx
+++ components/Dashboard/CreateMeal/AddMealButton.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import React from "react";
 import { useFormStatus } from "react-dom";
 const AddMealButton = ({
   text,
-  name
+  name,
+  className
 }: {
   text: string;
   name: string;
+  className?: string
 }) => {
   const { pending } = useFormStatus();
   return (
-    <Button disabled={pending} className="w-full flex-1" name={name} >
+    <Button disabled={pending} className={cn("w-full flex-1", className)} name={name} >
       {pending ? "Saving..." :  text}
     </Button>
   );
--- components/Dashboard/PreparedMeals/MealCard.tsx
+++ components/Dashboard/PreparedMeals/MealCard.tsx
@@ -11,11 +11,15 @@ import { QueryResultRow } from "@vercel/postgres";
 import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
+import { Button } from "@/components/ui/button";
+import { TiPlus } from "react-icons/ti";
+import AddMealButton from "../CreateMeal/AddMealButton";
 const MealCard = ({ meals }: { meals: QueryResultRow[] | undefined }) => {
   const mealList = meals?.map((meal) => (
     <Card
       key={meal.mealid}
-      className="max-h-[120px] overflow-hidden cursor-pointer relative group"
+      className="max-h-[120px] overflow-hidden  relative group"
     >
       <CardHeader className="space-y-0 ">
         <div>{meal.name}</div>
@@ -23,8 +27,10 @@ const MealCard = ({ meals }: { meals: QueryResultRow[] | undefined }) => {
       </CardHeader>
 
       <div className="absolute right-4 top-4 group-hover:flex gap-4 hidden">
-        <MdEdit className="  text-2xl text-black" />
-        <MdDelete className=" text-2xl text-red-600" />
+        <TiPlus className=" text-xl lg:text-2xl text-green-600 cursor-pointer" />
+        <MdEdit className=" text-xl lg:text-2xl text-black cursor-pointer" />
+        <MdDelete className=" text-xl lg:text-2xl text-red-600 cursor-pointer" />
+        <IoIosArrowDown className="text-xl lg:text-2xl text-black cursor-pointer" />
       </div>
     </Card>
   ));
