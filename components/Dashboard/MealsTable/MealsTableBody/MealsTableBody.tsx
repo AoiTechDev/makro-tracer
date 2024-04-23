832388da1180c8d69bcabe4cef41ed20105ec71f
Pawel <pawel.bornikowski@gmail.com>
2024-04-23 14:54:39 +0200

    fix(table): remove session prop

--- components/Dashboard/MealsTable/MealsTableBody/MealsTableBody.tsx
+++ components/Dashboard/MealsTable/MealsTableBody/MealsTableBody.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { TableBody } from "@/components/ui/table";
-import { Session } from "next-auth";
 import { getMeals } from "@/lib/getMeals/getMeals";
 
 import MealsTableRow from "./MealsTableRow/MealsTableRow";
 
-type MealsTableBodyProps = {
-  session: Session | null;
-};
-const MealsTableBody = async ({ session }: MealsTableBodyProps) => {
+
+const MealsTableBody = async () => {
   const result = await getMeals();
 
   return (
     <TableBody>
-      <MealsTableRow result={result} session={session} />
+      <MealsTableRow result={result}  />
     </TableBody>
   );
 };
