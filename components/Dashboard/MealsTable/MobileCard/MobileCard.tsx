4ec05b420906c133348a24c465a36098197d66f8
Pawel <pawel.bornikowski@gmail.com>
2024-04-23 14:59:45 +0200

    fix(mobileCard): remove session prop in mobile card

--- components/Dashboard/MealsTable/MobileCard/MobileCard.tsx
+++ components/Dashboard/MealsTable/MobileCard/MobileCard.tsx
@@ -1,14 +1,13 @@
 import { getMeals } from "@/lib/getMeals/getMeals";
 import React from "react";
 import MobileCardContent from "./MobileCardContent";
-import { getServerSession } from "next-auth";
 
 const MobileCard = async () => {
   const result = await getMeals();
-  const session = await getServerSession();
+
   return (
     <div className="w-full space-y-4 ">
-      <MobileCardContent result={result} session={session} />
+      <MobileCardContent result={result}  />
     </div>
   );
 };
